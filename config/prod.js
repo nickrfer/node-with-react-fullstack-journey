2c5601855204cc2a64e3a22ec523590042619888
Nicolas Ferreira <nickrfer@gmail.com>
2018-06-16 21:29:59 -0300

    Finish auth flow

--- config/prod.js
+++ config/prod.js
@@ -5,8 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-
-'810370364290-pr1gc7m7uva2nq2pi3dif77tp0hprfbm.apps.googleusercontent.com'
-'rasa-mtL7WaZzimnVt7XafpU',
-'mongodb://nickprod:123Mudar@@ds263710.mlab.com:63710/email-prod-nick',
