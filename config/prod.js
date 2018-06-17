a5c006d8b8e1868fc8e047412293d43c96a9165c
Nicolas Ferreira <nickrfer@gmail.com>
2018-06-16 21:11:28 -0300

    Finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+// prod.js - production keys!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+'810370364290-pr1gc7m7uva2nq2pi3dif77tp0hprfbm.apps.googleusercontent.com'
+'rasa-mtL7WaZzimnVt7XafpU',
+'mongodb://nickprod:123Mudar@@ds263710.mlab.com:63710/email-prod-nick',
